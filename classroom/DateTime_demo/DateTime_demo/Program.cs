5646bc4746ca798ab8f3cb09df323baeba83ddf0
JALoquacious <jalikoudis@protonmail.com>
2017-02-07 15:56:31 -0500

    Added DateTime demo and Dice Roller

---
+++ classroom/DateTime_demo/DateTime_demo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DateTime_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = GetDate();
+            bool isMonday = IsMonday(date);
+            DateTime friday = FindNextFriday(date).Date;
+            TimeSpan interval = friday - date;
+
+            Console.WriteLine(date.ToLongDateString());
+            Continue();
+
+            Console.WriteLine($"The date entered is {(isMonday ? "" : "not")} a Monday.");
+            Continue();
+
+            Console.WriteLine($"The next friday is {friday}.");
+            Continue();
+
+            Console.WriteLine($"The time interval in hours between {date} and");
+            Console.WriteLine($"its following Friday is {interval.TotalHours}.");
+            Continue();
+        }
+
+        static void Continue()
+        {
+            Console.WriteLine("Press <Enter> to continue...");
+            Console.ReadLine();
+        }
+
+        static DateTime GetDate()
+        {
+            DateTime date;
+            bool invalid = true;
+
+            do
+            {
+                Console.Write("Enter a given date (and time, if you wish): ");
+                string input = Console.ReadLine();
+
+                if (DateTime.TryParse(input, out date))
+                {
+                    invalid = false;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid input.");
+                }
+            } while (invalid);
+
+            return date;
+        }
+
+        static bool IsMonday(DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Monday;
+        }
+
+        static DateTime FindNextFriday(DateTime date)
+        {
+            do
+            {
+                date = date.AddDays(1);
+            } while (date.DayOfWeek != DayOfWeek.Friday);
+
+            return date;
+        }
+    }
+}
---
+++ classroom/Dice Roller/Dice Roller/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        private static Random _rng = new Random();
+
+        static void Main(string[] args)
+        {
+            foreach (var num in GetValues())
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+
+        public static Dictionary<byte, byte> GetValues()
+        {
+            Dictionary<byte, byte> rollCount = new Dictionary<byte, byte>();
+
+            for (byte i = 0; i < 100; i++)
+            {
+                byte roll = RollDice(_rng);
+
+                if (!rollCount.ContainsKey(roll))
+                {
+                    rollCount.Add(roll, 1);
+                }
+                else
+                {
+                    rollCount[roll]++;
+                }
+            }
+            return rollCount;
+        }
+
+        public static byte RollDice(Random _rng)
+        {
+            byte die1;
+            byte die2;
+
+            die1 = (byte)_rng.Next(1, 7);
+            die2 = (byte)_rng.Next(1, 7);
+
+            return (byte) (die1 + die2);
+        }
+    }
+}
