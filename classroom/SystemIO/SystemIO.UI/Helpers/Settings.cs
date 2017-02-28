69c7452c9af805d34f9cebaba387e024828e4bae
JALoquacious <jalikoudis@protonmail.com>
2017-02-28 13:11:47 -0500

    Moved classroom exercises from temp folder

---
+++ classroom/SystemIO/SystemIO.UI/Helpers/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIO.UI.Helpers
+{
+    public class Settings
+    {
+        public const string FilePath = @"..\SystemIO.UI\output\Students.txt";
+    }
+}
